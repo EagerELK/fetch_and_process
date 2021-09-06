16204d92839ed7263088a8d99b29141417fb9e64
Jurgens <jrgns@jadeit.co.za>
2021-09-06 23:22:34 +0200

    chore: Remove failing spec

--- spec/fetch_and_process_spec.rb
+++ spec/fetch_and_process_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe FetchAndProcess do
   it 'has a version number' do
     expect(FetchAndProcess::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(true).to eq(false)
-  end
 end
